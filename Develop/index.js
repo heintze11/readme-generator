5581d70e122e6fce2e8af626f80930aeb032ed05
Levi H <levi.heintzelman@gmail.com>
2022-08-17 16:10:44 -0600

    Added basic questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the project title?", "What is the description of the project?", "Installation", "Usage", "Contribution Guidlines", "Test Instructions", "What is your github username?", "What is your email?", "License"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
