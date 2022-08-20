541529f11a9a364bccb56ddf2446c3635b5b90f4
Levi H <levi.heintzelman@gmail.com>
2022-08-20 12:44:14 -0600

    Fixed questions format, added init function

--- Develop/index.js
+++ Develop/index.js
@@ -5,13 +5,58 @@ const generateMarkdown = require("./utils/generateMarkdown.js")
 
 
 // TODO: Create an array of questions for user input
-const questions = ["What is the project title?", "What is the description of the project?", "Installation", "Usage", "Contribution Guidlines", "Test Instructions", "What is your github username?", "What is your email?", "License"];
+const questions = [{
+    type: "input",
+    message: "What is the project title?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is the description of the project?",
+    name: "Description"
+}, {
+    type: "input",
+    message: "Installation",
+    name: "Installation"
+}, {
+    type: "input",
+    message: "Usage",
+    name: "Usage"
+}, { 
+    type: "input",
+    message: "Contribution Guidlines",
+    name: "Contribution"
+ }, {
+    type: "input",
+    message: "Test Instructions",
+    name: "Instructions"
+  },{
+    type: "input",
+    message: "What is your github username?",
+    name: "Github"
+   }, {
+    type: "input"
+    message: "What is your email?",
+    name: "email"
+    }, {
+    type: "list"
+    message: "What license was used?"
+    choices: ["MIT", "GPL", "BSD"]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(info) {
+        writeToFile("ReadmeTest.md", generateMarkdown(info));
+
+    }) 
+}
 
 // Function call to initialize app
 init();
